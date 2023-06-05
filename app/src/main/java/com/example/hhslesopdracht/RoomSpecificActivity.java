f680335dd8b634f93787bd2617eeb2809bb8457a
dragondungeon1 <lennartderidder@gmail.com>
2023-06-05 21:15:06 +0200

    feat: added 2 screens for APEP project ~will be removed later

---
+++ app/src/main/java/com/example/hhslesopdracht/RoomSpecificActivity.java
@@ -0,0 +1,14 @@
+package com.example.hhslesopdracht;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class RoomSpecificActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_room_specific);
+    }
+}
